d72a2ed6b42f0ec0b62c06c70b071760da7f41a1
Elvis Moses Tangasaiti <elvis.moses@student.moringaschool.com>
2024-05-10 13:06:13 +0300

    creates App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import  Products from './components/Products'
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import Details from "./components/Details"
-import { Navbar } from "./components/Navbar";
-import { CartProvider } from './context/cart.jsx'
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import BrowserRouter, Routes, and Route
+import Products from './components/Products';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Details from './components/Details';
+import { Navbar } from './components/Navbar';
+import { CartProvider } from './context/cart';
 
 function App() {
   return (
     <div>
       <CartProvider>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Products />} />
-          <Route path="/login"  element={<Login/>} />
-          <Route path="/details"  element={<Details />} />
-          <Route path="/register"  element={<Register/>} />
-        </Routes>
-      </Router>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Products />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/details" element={<Details />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </Router>
       </CartProvider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
